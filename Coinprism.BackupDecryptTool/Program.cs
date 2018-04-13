17e5023e20c074c7dcf405c2eb6a492c2c8a8a08
Flavien Charlon <flavien@charlon.net>
2018-04-13 21:05:47 +0100

    Create the solution

---
+++ Coinprism.BackupDecryptTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coinprism.BackupDecryptTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
